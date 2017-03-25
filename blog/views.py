ccabeb4f84ff7935f826157a5dcb5c8424a134c1
Ravil Bikbulatov <weeellz12@gmail.com>
2017-03-25 22:16:58 +0300

    added post details, fixed no reverse match error

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,12 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	for post in posts:
+		if len(post.text) > 50:
+			post.text = post.text[:50] + '...'
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, key):
+	details = Post.objects.get(pk=key)
+	return render(request, 'blog/post_detail.html', {'details': details})
