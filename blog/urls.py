6c0f1b204704db66235801e8d7ec28a0a79fcf63
Ravil Bikbulatov <weeellz12@gmail.com>
2017-03-26 00:54:43 +0300

    now you can add new post and change it if you are authorized, added forms, post_edit

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^post_detail/(?P<key>[0-9]+)$', views.post_detail, name='post_detail')
-]
+	url(r'^post_detail/(?P<key>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^new_post/$', views.new_post, name='new_post'),
+	url(r'^post_edit/(?P<key>[0-9]+)/$', views.post_edit, name='post_edit')
+	]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -13,3 +14,30 @@ def post_list(request):
 def post_detail(request, key):
 	details = Post.objects.get(pk=key)
 	return render(request, 'blog/post_detail.html', {'details': details})
+
+def new_post(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', key=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, key):
+	post = Post.objects.get(pk=key)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', key=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
